61b53358f096b0f84e2ad771500d7cc00517bfeb
JonMaslow <114214959+JonMaslow@users.noreply.github.com>
2022-10-18 02:03:27 +0500

    task 3 принимаем и выдаём массив с помощью метода string.Split()

---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+
+ввод элеменетов через консоль (через запятую. использовать метод string.Split())
+*/
+int [] GetArray(string message)
+{
+    Console.WriteLine(message);
+    string str = Console.ReadLine()??"";
+    string [] strArray = str.Split(',');
+    int [] numArray = new int[strArray.Length];
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        numArray[i] = int.Parse(strArray[i]);    
+    }
+    return numArray;
+}
+
+int [] resultArray = GetArray("Введите числа через запятую:");
+Console.WriteLine("Итоговый массив:");
+Console.Write("[");
+
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < resultArray.Length; i++)
+    { 
+        if (i < resultArray.Length - 1)
+            Console.Write($"{resultArray[i]}, ");
+        else
+            Console.Write($"{resultArray[i]}");
+    }
+}
+PrintArray(resultArray);
+Console.Write("]");
